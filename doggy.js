477061ceeb84a7c9e97d641c4ab735581ae58516
oryxwhite <93627129+oryxwhite@users.noreply.github.com>
2021-11-25 00:36:38 -0500

    fixed bug not allowing videos

--- doggy.js
+++ doggy.js
@@ -5,5 +5,21 @@ fetch('https://random.dog/woof.json')
     })
 
     .then(function(response) {
-        img.src=response.url
+        let displayImg = document.createElement("img")
+        displayImg.className = "dogpic";
+        displayImg.src = response.url;
+
+        let displayVid = document.createElement("video");
+        displayVid.className = 'dogpic';
+        displayVid.autoplay = true;
+        displayVid.src = response.url
+        displayVid.muted = true;
+
+        let ext = response.url.split('.').pop();
+        if(ext === 'mp4') {
+            document.getElementById("maincont").appendChild(displayVid);
+
+        } else {
+            document.getElementById("maincont").appendChild(displayImg);
+        }
     })
